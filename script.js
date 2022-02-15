b1f0f2043962688a01451eefe300c4c5658a07ff
mmgonzalez <mmgonzalez42@gmail.com>
2022-02-14 22:39:30 -0800

    create individual case project ch 6

---
+++ script.js
@@ -0,0 +1,5 @@
+
+console.log("Web browser name: " + navigator.appName);
+console.log("Web browser version: " + navigator.appVersion);
+console.log("Operating Platform: " + navigator.platform);
+console.log("User agent: " + navigator.userAgent);
